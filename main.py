17b802fb6bb1eb4b722d33f31adbdda33d80fe9e
RomoR11 <rm.mikhanov@gmail.com>
2024-04-21 19:29:45 +0300

    Теперь можно выиграть!

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from data.users import User
 from data.bets import Bets
 from data import db_session
 import datetime as dt
+import time as t
 
 
 app = Flask(__name__)
@@ -21,14 +22,7 @@ codes_competitions = {'Premier League': 'PL', 'Primera Division': 'PD', 'Ligue 1
 login_manager = LoginManager()
 login_manager.init_app(app)
 db_session.global_init("db/users.db")
-# user = Bets()
-# user.match_id = "444566"
-# user.bet = ""
-# user.email = "email@email.ru"
-# user.user_id = 1
-# db_sess = db_session.create_session()
-# db_sess.add(user)
-# db_sess.commit()
+USERNAME = ''
 
 
 @login_manager.user_loader
@@ -42,11 +36,17 @@ def start():
     matches = []
     time_from, time_to = dt.datetime.now().date(), dt.datetime.now().date() + dt.timedelta(days=5)
     response = requests.get(url=f'{url}/matches/?dateFrom={time_from}&dateTo={time_to}', headers=headers).json()
+    if 'message' in response.keys():
+        t.sleep(int(response['message'][-11:-9]))
+        response = requests.get(url=f'{url}/matches/?dateFrom={time_from}&dateTo={time_to}', headers=headers).json()
     for i in response['matches']:
         if i['competition']['code'] in codes_competitions.values():
             date, time = f"{i['utcDate'].split('T')[0]}", f"{i['utcDate'].split('T')[1][:5]}"
-            matches.append((i["homeTeam"]["shortName"], i["homeTeam"]["crest"],
-                            i["awayTeam"]["shortName"], i["awayTeam"]["crest"], (date, time), i['id']))
+            cr_date = dt.datetime.strptime(date + time, '%Y-%m-%d%H:%M')
+            date_now = dt.datetime.now()
+            if cr_date > date_now:
+                matches.append((i["homeTeam"]["shortName"], i["homeTeam"]["crest"],
+                                i["awayTeam"]["shortName"], i["awayTeam"]["crest"], (date, time), i['id']))
     return render_template('main.html', matches=matches, length=len(matches))
 
 
@@ -54,6 +54,9 @@ def start():
 def bet(match):
     form = BetForm()
     response = requests.get(url=f'{url}/matches/{match}', headers=headers).json()
+    if 'message' in response.keys():
+        t.sleep(int(response['message'][-11:-9]))
+        response = requests.get(url=f'{url}/matches/{match}', headers=headers).json()
     form.bet.choices = [(response["homeTeam"]["shortName"], response["homeTeam"]["shortName"]),
                         ('Ничья', 'Ничья'),
                         (response["awayTeam"]["shortName"], response["awayTeam"]["shortName"])]
@@ -70,6 +73,7 @@ def bet(match):
         user_bet.bet = form.bet.data
         user_bet.bet_money = form.bet_money.data
         db_sess.add(user_bet)
+        user.amount_of_money -= int(form.bet_money.data)
         db_sess.commit()
         return redirect('/')
     return render_template('create_bet.html', form=form)
@@ -90,6 +94,9 @@ def competition(code):
     if code in codes_competitions.values():
         teams = []
         response = requests.get(url=f'{url}/competitions/{code}/standings', headers=headers).json()['standings'][0]
+        if 'message' in response.keys():
+            t.sleep(int(response['message'][-11:-9]))
+            response = requests.get(url=f'{url}/competitions/{code}/standings', headers=headers).json()['standings'][0]
         for team in response['table']:
             teams.append((team['position'], team['team']['shortName'], team['team']['crest'], team['playedGames'],
                           team['won'], team['draw'], team['lost'], team['points']))
@@ -105,12 +112,13 @@ def logout():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global USERNAME
     form = LoginForm()
-    print(form.validate_on_submit())
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.name == form.name.data).first()
         if user and user.check_password(form.password.data):
+            USERNAME = form.name.data
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
         return render_template('login.html',
@@ -140,16 +148,42 @@ def register():
     return render_template('register.html', title='Регистрация', form=form)
 
 
+def win_bet(money, bet):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.name == USERNAME).first()
+    user.amount_of_money += int(money) * 2 if bet != 'Ничья' else int(money) + int(money) // 2
+    db_sess.commit()
+
+
 @app.route('/user_bets')
 def user_bets():
+    global USERNAME
     db_sess = db_session.create_session()
     bets = []
-    for i in db_sess.query(Bets).filter(Bets.user_id == User.id):
+    user = db_sess.query(User).filter(User.name == USERNAME).first()
+    for i in db_sess.query(Bets).filter(Bets.user_id == user.id):
+        win = 0
         response = requests.get(url=f'{url}/matches/{i.match_id}', headers=headers).json()
+        if 'message' in response.keys():
+            t.sleep(int(response['message'][-11:-9]))
+            response = requests.get(url=f'{url}/matches/{i.match_id}', headers=headers).json()
         date = f"{response['utcDate'].split('T')[0]} {response['utcDate'].split('T')[1][:5]}"
+        if (i.bet == response["homeTeam"]["shortName"] and response["score"]["winner"] == 'HOME_TEAM' or
+                i.bet == response["awayTeam"]["shortName"] and response["score"]["winner"] == 'AWAY_TEAM' or
+                i.bet == 'Ничья' and response["score"]["winner"] == 'DRAW') and \
+                response["status"] == 'FINISHED':
+            win_bet(i.bet_money, i.bet)
+            win = 1
+        elif response["status"] != 'FINISHED':
+            win = 0
+        else:
+            win = 2
         bets.append((response["homeTeam"]["shortName"], response["homeTeam"]["crest"],
                      response["awayTeam"]["shortName"], response["awayTeam"]["crest"],
-                     date, response["score"]["winner"], i.bet))
+                     date, win, i.bet, i.bet_money))
+        if response["status"] == 'FINISHED':
+            db_sess.delete(i)
+            db_sess.commit()
     return render_template("bets.html", bets=bets, length=len(bets))
 
 
